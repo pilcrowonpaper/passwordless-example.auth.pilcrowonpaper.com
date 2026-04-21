8e99d2eece78870792d81bb84d70c0865059db18
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2026-04-21 11:37:16 +0900

    fix og description

--- pages.go
+++ pages.go
@@ -532,7 +532,7 @@ func createPageHTML(requestId string, title string, bodyHTML string, script stri
 	<meta property="og:type" content="website" />
 	<meta property="og:locale" content="en_US" />
 	<meta property="og:site_name" content="Passwordless auth example" />
-	<meta property="og:description" content="An example website that implements email address and password authentication following best practices." />
+	<meta property="og:description" content="An example website that implements email code sign-in and passkeys following best practices." />
 	<meta property="og:url" content="https://passwordless-example.auth.pilcrowonpaper.com" />
 	<meta property="og:image" content="https://pilcrowonpaper.com/profile.jpg" />
 
